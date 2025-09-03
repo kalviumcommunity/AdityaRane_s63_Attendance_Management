1e3b16191c6d595ae9262f54fa8e2813431f514c
Haschwalt29 <aditya.rane.s63@kalvium.community>
2025-09-03 11:22:42 +0530

    feat: Added new changes for part 1

--- src/com/school/Main.java
+++ src/com/school/Main.java
@@ -2,8 +2,17 @@ package com.school;
 
 public class Main {
     public static void main(String[] args) {
+        displayWelcomeMessage();
+        displayProjectInfo();
+    }
+    
+    private static void displayWelcomeMessage() {
         System.out.println("Welcome to Attendance Management System!");
+    }
+    
+    private static void displayProjectInfo() {
         System.out.println("This is Part 1 of the project setup.");
         System.out.println("Project structure created successfully!");
+        System.out.println("Ready for development!");
     }
 }
